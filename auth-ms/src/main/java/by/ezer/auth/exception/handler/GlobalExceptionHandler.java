e43e012e5f34fda75c5759e5705ae0967c78a2cb
Edward <pakachun48@gmail.com>
2026-03-11 19:44:29 +0300

    add migrations

--- auth-ms/src/main/java/by/ezer/auth/exception/handler/GlobalExceptionHandler.java
+++
@@ -1,7 +0,0 @@
-package by.ezer.auth.exception.handler;
-
-public class GlobalExceptionHandler extends RuntimeException {
-    public GlobalExceptionHandler(String message) {
-        super(message);
-    }
-}

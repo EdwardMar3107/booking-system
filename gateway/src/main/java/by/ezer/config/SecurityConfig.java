3010f6151138efe8c200f170ee323bf7a8798b24
Edward <pakachun48@gmail.com>
2026-03-12 21:57:56 +0300

    feat: add SecurityConfig

---
+++ gateway/src/main/java/by/ezer/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package by.ezer.config;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/auth/**").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .build();
+    }
+}

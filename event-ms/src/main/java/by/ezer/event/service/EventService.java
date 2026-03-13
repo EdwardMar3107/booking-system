647f43cbd71e2767b8479805223f9a6d45b403e0
Edward <pakachun48@gmail.com>
2026-03-13 21:10:03 +0300

    feat: add EventMapper, changed EventService with mapper

--- event-ms/src/main/java/by/ezer/event/service/EventService.java
+++ event-ms/src/main/java/by/ezer/event/service/EventService.java
@@ -3,6 +3,7 @@ package by.ezer.event.service;
 import by.ezer.event.dto.CreateEventRequest;
 import by.ezer.event.dto.EventResponse;
 import by.ezer.event.entity.Event;
+import by.ezer.event.mapper.EventMapper;
 import by.ezer.event.repository.EventRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,26 +13,15 @@ import org.springframework.stereotype.Service;
 public class EventService {
 
     private final EventRepository eventRepository;
+    private final EventMapper eventMapper;
 
     public EventResponse createEvent(CreateEventRequest request, String userEmail) {
 
-        Event event = Event.builder()
-                .title(request.title())
-                .description(request.description())
-                .location(request.location())
-                .eventDate(request.eventDate())
-                .createdBy(userEmail)
-                .build();
+        Event event = eventMapper.toEntity(request, userEmail);
 
         Event savedEvent = eventRepository.save(event);
 
-        return new EventResponse(
-                savedEvent.getId(),
-                savedEvent.getTitle(),
-                savedEvent.getDescription(),
-                savedEvent.getLocation(),
-                savedEvent.getEventDate(),
-                savedEvent.getCreatedBy()
-        );
+        return eventMapper.toResponse(savedEvent);
+
     }
 }

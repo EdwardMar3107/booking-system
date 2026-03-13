f08258be975626afb9248a56fe78e469491e952e
Edward <pakachun48@gmail.com>
2026-03-13 19:11:37 +0300

    feat: add entity Event

---
+++ event-ms/src/main/java/by/ezer/event/entity/Event.java
@@ -0,0 +1,45 @@
+package by.ezer.event.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "events")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Event {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false, length = 200)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(nullable = false, length = 225)
+    private String location;
+
+    @Column(name = "event_date", nullable = false)
+    private LocalDate EventDate;
+
+    @Column(name = "created_by", nullable = false, length = 255)
+    private String createdBy;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = LocalDateTime.now();
+    }
+}

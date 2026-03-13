c97d770c370b2f6e26caedabe207eb137f9d9881
Edward <pakachun48@gmail.com>
2026-03-13 19:31:25 +0300

    feat: add EventRepository

---
+++ event-ms/src/main/java/by/ezer/event/repository/EventRepository.java
@@ -0,0 +1,14 @@
+package by.ezer.event.repository;
+
+import by.ezer.event.entity.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+public interface EventRepository extends JpaRepository<Event, UUID> {
+    List<Event> findByCreatedBy(String createdBy);
+    List<Event> findByEventDate(LocalDate eventDate);
+    List<Event> findByCreatedByAndEventDate(String createdBy, LocalDate eventDate);
+}

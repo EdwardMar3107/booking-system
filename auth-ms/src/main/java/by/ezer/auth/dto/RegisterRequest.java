9605695291d6cd4abf465cc1be43047b4dee6bb9
Edward <pakachun48@gmail.com>
2026-03-19 20:38:09 +0300

    refactor: fixed @Size

--- auth-ms/src/main/java/by/ezer/auth/dto/RegisterRequest.java
+++ auth-ms/src/main/java/by/ezer/auth/dto/RegisterRequest.java
@@ -8,10 +8,10 @@ public record RegisterRequest(
 
         @Email(message = "Please provide a valid email address")
         @NotBlank(message = "Email is required")
-        @Size(min = 12, max = 100, message = "Email must be between 5 and 100 characters")
+        @Size(min = 12, max = 100, message = "Email must be between 12 and 100 characters")
         String email,
 
         @NotBlank(message = "Password is required")
-        @Size(min = 6, max = 100, message = "Password must be between 8 and 100 characters")
+        @Size(min = 6, max = 100, message = "Password must be between 6 and 100 characters")
         String password) {
 }

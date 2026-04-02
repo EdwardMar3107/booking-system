5cf3ed6cf18b59574a65400a98fa4298c676e16a
Edward <pakachun48@gmail.com>
2026-04-02 23:04:21 +0300

    chore: add routes in gateway

--- gateway/src/main/java/by/ezer/gateway/GatewayApplication.java
+++ gateway/src/main/java/by/ezer/gateway/GatewayApplication.java
@@ -1,6 +1,11 @@
-package by.ezer;
+package by.ezer.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class GatewayApplication {
     static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
